840d1752ee9454588c5146d501fc0878a7f9b4a5
Kamil Stepczuk <k.stepczuk@hotmail.com>
2017-04-27 14:04:17 +0200

    CORS support.

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ mongoose.connect(mongolabUri);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 const routes = require('./api/routers/event');
 routes(app);
